d8dcd26a6ae17c80575bf27b89fe64fdf4f90fc9
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-09 16:46:10 +0200

    _app configuration, build command

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps: { session, ...pageProps}}) {
+  // return <Component {...pageProps} />;
+  return (
+    <>
+    <html lang="en" suppressHydrationWarning>
+      <ThemeProvider attribute="class" defaultTheme="system">
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+      </ThemeProvider>
+    </html>
+    </>
+  )
 }
