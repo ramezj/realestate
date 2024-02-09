c5d8310d35205fbd89fe4259a4d37f4007c934f5
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-10 01:40:43 +0200

    button,input, label from shadcn, new SearchBar component

---
+++ app/components/SearchBar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Input } from './ui/input'
+
+export const SearchBar = () => {
+  return (
+    <Input placeholder='District, town, or city' className='shadow-md'/>
+  )
+}
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,9 +1,18 @@
 import Layout from "@/components/Layout";
+import { SearchBar } from "@/components/SearchBar";
 
 export default function Home() {
   return (
     <>
     <Layout>
+      <center>
+          <h1 className="font-bold text-4xl mt-16">
+            Ideal Real Estate Matches for Everybody, Everywhere.
+          </h1>
+        <div className="2xl:w-1/3 lg:w-1/2 md:w-1/2 w-3/4 mt-16">
+          <SearchBar />
+        </div>
+      </center>
     </Layout>
     </>
   );
