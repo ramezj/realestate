81d6f44de27377eafb2c801ecc4bee66a10dfc44
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-14 12:07:49 +0200

    search bar in /search page [ not working yet ]

--- app/pages/search/index.js
+++ app/pages/search/index.js
@@ -3,12 +3,21 @@ import { useSearchParams, useRouter } from "next/navigation"
 import { PropertyCard } from "@/components/PropertyCard";
 import Layout from "@/components/Layout";
 import { useSession } from "next-auth/react";
+import { SearchBar } from "@/components/SearchBar";
+import { Button } from "@/components/ui/button";
 
 export default function SearchPage() {
+    const router = useRouter();
+    const [ searchQ, setSearchQ ] = useState("");
     const { data: session, status } = useSession()
     const [ loading, setLoading ] = useState(true);
     const [ response, setResponse ] = useState();
     const [ properties, setProperties ] = useState();
+    const onSearch = (e) => {
+        e.preventDefault();
+        const encodedSearchQuery = encodeURI(searchQ);
+        router.push(`/search?q=${encodedSearchQuery}`)
+    }
     const search = useSearchParams();
     const searchQuery = search.get('q');
     const encodedSearchQuery = encodeURI(searchQuery || "");
@@ -25,6 +34,18 @@ export default function SearchPage() {
     }, [])
     return (
         <Layout session={session}>
+            <br />
+            <center>
+                <div className="md:w-3/4 mt-8">
+                <form onSubmit={onSearch}>
+                <SearchBar value={searchQ} onChange={((e) => { setSearchQ(e.target.value)})} />
+                <br />
+                <Button type='submit' className='w-[15rem]'>
+                Find your new home
+                </Button>
+                </form>
+                </div>
+            </center>
         <div className="flex flex-wrap gap-8 justify-center w-full">
         { 
         loading 
